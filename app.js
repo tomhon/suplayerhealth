82f0d81e7afb3fc23a3b9ef3f96a4fd9c48d1d65
Tom Honeybone <tomhon@microsoft.com>
2017-05-18 14:29:28 -0700

    Added docdb logging

--- app.js
+++ app.js
@@ -1,11 +1,105 @@
 var restify = require('restify');
 var builder = require('botbuilder');
+var config = require("./config");
+
+var documentClient = require("documentdb").DocumentClient;
+var config = require("./config");
+var url = require('url');
+
+var client = new documentClient(config.endpoint, { "masterKey": config.primaryKey });
+
+var HttpStatusCodes = { NOTFOUND: 404 };
+var databaseUrl = `dbs/${config.database.id}`;
+var collectionUrl = `${databaseUrl}/colls/${config.collection.id}`;
+
+
+
 
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
+
+/**
+ * Get the database by ID, or create if it doesn't exist.
+ * @param {string} database - The database to get or create
+ */
+function getDatabase() {
+    console.log(`Getting database:\n${config.database.id}\n`);
+
+    return new Promise((resolve, reject) => {
+        client.readDatabase(databaseUrl, (err, result) => {
+            if (err) {
+                if (err.code == HttpStatusCodes.NOTFOUND) {
+                    client.createDatabase(config.database, (err, created) => {
+                        if (err) reject(err)
+                        else resolve(created);
+                    });
+                } else {
+                    reject(err);
+                }
+            } else {
+                resolve(result);
+            }
+        });
+    });
+}
+
+/**
+ * Get the collection by ID, or create if it doesn't exist.
+ */
+function getCollection() {
+    console.log(`Getting collection:\n${config.collection.id}\n`);
+
+    return new Promise((resolve, reject) => {
+        client.readCollection(collectionUrl, (err, result) => {
+            if (err) {
+                if (err.code == HttpStatusCodes.NOTFOUND) {
+                    client.createCollection(databaseUrl, config.collection, { offerThroughput: 400 }, (err, created) => {
+                        if (err) reject(err)
+                        else resolve(created);
+                    });
+                } else {
+                    reject(err);
+                }
+            } else {
+                resolve(result);
+            }
+        });
+    });
+}
+
+/**
+ * Get the document by ID, or create if it doesn't exist.
+ * @param {function} callback - The callback function on completion
+ */
+function getDBDocument(document) {
+    let documentUrl = `${collectionUrl}/docs/${document.id}`;
+    console.log(`Getting document:\n${document.id}\n`);
+
+    return new Promise((resolve, reject) => {
+        client.readDocument(documentUrl, (err, result) => {
+            if (err) {
+                if (err.code == HttpStatusCodes.NOTFOUND) {
+                    client.createDocument(collectionUrl, document, (err, created) => {
+                        if (err) reject(err)
+                        else resolve(created);
+                    });
+                } else {
+                    reject(err);
+                }
+            } else {
+                resolve(result);
+            }
+        });
+    });
+};
+
+function pushLogData (document) {
+    logDataArray.push(document);
+    console.log('log pushed to array')
+}
 
 // List the questions to be asked
 var questionsList = Array ();
@@ -22,10 +116,12 @@ questionsList[9] = 'What was the objective of the Session? (text)';
 questionsList[10] = 'What did you learn or improve? (text)';
 
 function logData (session, question, response) {
-    this.timestamp = null,
-    this.user = null,
-    this.question = null,
-    this.response = null
+    // id is the timestamp
+    var date = new Date();
+    this.id = date.getTime().toString(),
+    this.user = 'Admin',
+    this.question = 'Bot Initialized',
+    this.response = 'Successfully'
 };
 
 var logDataArray = Array();
@@ -40,12 +136,15 @@ console.log(numberPromptOptions);
 
 function logResponse (session, question, results) {
         oLogData = new logData();
-        oLogData.timestamp = session.message.timestamp;
         oLogData.user = session.message.user.name;
         oLogData.question = question.slice(0,question.indexOf('?')+1);
         oLogData.response = results.response;
-        logDataArray.push(oLogData);
-        console.log(oLogData);
+        console.log('attempting write to docdb');
+        getDBDocument(oLogData)
+            .then(()  =>   console.log(oLogData))
+            .catch((error) => { exit(`Completed with error ${JSON.stringify(error)}`) });
+
+
 }
 
 
@@ -65,68 +164,58 @@ var bot = new builder.UniversalBot(connector, [
         builder.Prompts.number( session, questionsList[0], numberPromptOptions );
         },
     function (session, results, next) {
-        session.send('Great - thanks');
         logResponse(session, questionsList[0], results);
         builder.Prompts.number( session, questionsList[1], numberPromptOptions );
         next();
     },
         function (session, results, next) {
-        session.send('Great - thanks');
         logResponse(session, questionsList[1], results);
         builder.Prompts.number( session, questionsList[2], numberPromptOptions );
         next();
     },
         function (session, results, next) {
-        session.send('Great - thanks');
         logResponse(session, questionsList[2], results);
         builder.Prompts.number( session, questionsList[3], numberPromptOptions );
         next();
     },
         function (session, results, next) {
-        session.send('Great - thanks');
         logResponse(session, questionsList[3], results);
         builder.Prompts.number( session, questionsList[4], numberPromptOptions );
         next();
     },
         function (session, results, next) {
-        session.send('Great - thanks');
         logResponse(session, questionsList[4], results);
         builder.Prompts.number( session, questionsList[5], numberPromptOptions );
         next();
     },
         function (session, results, next) {
-        session.send('Great - thanks');
         logResponse(session, questionsList[5], results);
         builder.Prompts.number( session, questionsList[6], numberPromptOptions );
         next();
     },
         function (session, results, next) {
-        session.send('Great - thanks');
         logResponse(session, questionsList[6], results);
         builder.Prompts.number( session, questionsList[7], numberPromptOptions );
         next();
     },
         function (session, results, next) {
-        session.send('Great - thanks');
         logResponse(session, questionsList[7], results);
         builder.Prompts.number( session, questionsList[8], numberPromptOptions );
         next();
     },
         function (session, results, next) {
-        session.send('Great - thanks');
         logResponse(session, questionsList[8], results);
         builder.Prompts.text( session, questionsList[9], textPromptOptions );
         next();
     },
         function (session, results, next) {
-        session.send('Great - thanks');
         logResponse(session, questionsList[9], results);
         builder.Prompts.text( session, questionsList[10], textPromptOptions );
         next();
     },
 
     function (session, results) {
-        session.send('Great - thanks');
+        session.send('Thanks for all your answers. See you soon!');
         logResponse(session, questionsList[10], results);
         if (!results.response) {
             // exhausted attemps and no selection, start over
@@ -157,3 +246,12 @@ server.get('/', restify.serveStatic({
  directory: __dirname,
  default: '/index.html',
 }));
+
+initialLogEntry = new logData();
+
+
+//initialize database
+getDatabase()
+     .then(() => getCollection())
+     .then(() => {getDBDocument(initialLogEntry)})
+     .catch((error) => { exit(`Completed with error ${JSON.stringify(error)}`) });
